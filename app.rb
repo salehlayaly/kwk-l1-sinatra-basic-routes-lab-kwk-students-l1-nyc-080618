cbc892be43b8164f2d93475671fae6c9f7a57744
salehlayaly <lsaleh@hcstonline.org>
2018-08-15 13:30:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-get ""
-
-
+  get "/name"
+  "My name is__"
+  end 
+  get "/hometown" 
+  "My hometown is__"
+  end 
+  get "/favorite-song"
+  "My favorite song is__"
+  end
 end
 
